6639d4ad6249a8a7824d903a86c228c0517fe878
danghai1412 <haipdgch200436@fpt.edu.vn>
2022-10-01 15:30:01 +0700

    ok

---
+++ app.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+const path = require("path");
+
+const mongoose = require("mongoose");
+const studentModel = require("./models/StudentSchema");
+const url = "mongodb+srv://danghai1412:hai14122002@cluster0.b95irw9.mongodb.net/greenwich";
+mongoose.connect(url, { useNewUrlParser: true }, (err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("connect to db succeed !");
+  }
+});
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "hbs");
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.get("/about", function (req, res) {
+  res.render("about");
+});
+
+app.get("/student", (req, res) => {
+  studentModel.find((err, data) => {
+    if (err) {
+      console.log(err);
+    } else {
+      //1. show dữ liệu ra console log
+      //console.log(data);
+      //2. show dữ liệu ra API bằng "send"
+      //res.send(data);
+      //3. show dữ liệu ra view bằng "render"
+      res.render("student", { students: data });
+    }
+  });
+});
+
+app.listen(port);
