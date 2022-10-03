1e53280a3397b697ebab3ed6b2035c7997b1a588
danghai1412 <haipdgch200436@fpt.edu.vn>
2022-10-03 19:52:48 +0700

    ok

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
+//module import
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 const path = require("path");
 
+//database connect + access
 const mongoose = require("mongoose");
 const studentModel = require("./models/StudentSchema");
-const url = "mongodb+srv://danghai1412:hai14122002@cluster0.b95irw9.mongodb.net/greenwich";
+//url connection string (local db)
+const url = "mongodb://localhost:27017/greenwich";
+//url connection string (cloud db)
+//const url =
 mongoose.connect(url, { useNewUrlParser: true }, (err) => {
   if (err) {
     console.log(err);
@@ -13,6 +18,40 @@ mongoose.connect(url, { useNewUrlParser: true }, (err) => {
     console.log("connect to db succeed !");
   }
 });
+
+
+//body-parser: get input from FORM
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: false }));
+
+//render ra form add
+app.get("/add", (req, res) => {
+    res.render("add");
+  });
+  
+  //nhận và xử lý dữ liệu từ form add
+  app.post("/add", (req, res) => {
+   //console.log(req.body);
+  //res.send(req.body);
+  //res.render("output", { student: req.body });
+
+  //tạo object student chứa dữ liệu nhập từ form
+  var student = new studentModel({
+    name: req.body.name,
+    age: req.body.age,
+    email: req.body.email,
+    image: req.body.image,
+  });
+  //lưu object student vào database
+  student.save((err) => {
+    if (err) {
+      console.error(err);
+    } else {
+      res.redirect("/student");
+    }
+  });
+  });
+
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "hbs");
@@ -24,6 +63,22 @@ app.get("/", (req, res) => {
 app.get("/about", function (req, res) {
   res.render("about");
 });
+
+app.get("/login", (req, res) => {
+    res.render("login");
+  });
+
+  app.post("/login", (req, res) => {
+    var username = req.body.username;
+    var password = req.body.password;
+    var check = "Login failed";
+    if (username == "admin" && password == "123456") {
+      check = "Login succed !";
+    }
+    //res.send(check);
+    res.render("check", { result: check });
+})
+
 
 app.get("/student", (req, res) => {
   studentModel.find((err, data) => {
